162137fb82a65e29b4a6659a568cd48e4d1aeb65
Michael Bade <mbade314@gmail.com>
2020-05-22 17:14:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -24,7 +24,7 @@ class Triangle
   end
 
   class TriangleError < StandardError
-    "The lengths of 2 sides are shorter than 1 side of the triangle, or one of your sides has a negative input."
+    "The sum of 2 sides are shorter than 1 side of the triangle, or one side is a negative number."
   end
 
 end
