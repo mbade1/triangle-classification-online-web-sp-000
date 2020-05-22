502592eebc3ceb1c679ccda6003806891eae80f6
Michael Bade <mbade314@gmail.com>
2020-05-22 17:01:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,4 +6,25 @@ class Triangle
     @side2 = side2
     @side3 = side3
   end
+
+  def kind
+    if (@side1 > 0 && @side2 > 0 && @side3 > 0)
+      true
+    else
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    end
+    if 
+    elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
+      return :isosceles
+    elsif (@side1 == @side2) && (@side2 == @side3)
+      return :equilateral
+    else
+      return :scalene
+    end
+  end
+      
 end
